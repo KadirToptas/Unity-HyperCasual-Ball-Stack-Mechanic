8458428af51ac2c4714a7757c350472404f5c23f
KadirToptas <kdrrtptss@gmail.com>
2024-06-27 00:01:00 +0300

    Ball-Stack

--- Assets/Scripts/Ball_Controller.cs
+++ Assets/Scripts/Ball_Controller.cs
@@ -4,9 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class Ball_Controller : MonoBehaviour
 {
+    [SerializeField] private GameObject _ballPrefab;
+    
+    private int _gateNumber;
+
+    private int _targetCount;
+    
     [Header("TEXT")]
     [SerializeField] private TMP_Text _ballCountText;
     
@@ -64,6 +71,44 @@ public class Ball_Controller : MonoBehaviour
             other.gameObject.GetComponent<SphereCollider>().enabled = false;
             other.gameObject.transform.localPosition = new Vector3(0f, 0f, _balls[_balls.Count - 1].transform.localPosition.z -1f);
             _balls.Add(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Gate"))
+        {
+            _gateNumber = other.gameObject.GetComponent<Gate_Controller>().GetGateNumber();
+            _targetCount = _balls.Count + _gateNumber;
+
+
+            if (_gateNumber > 0)
+            {
+              IncreaseBallCount();  
+            }
+            else if(_gateNumber <0){
+                DecreaseBallCount();
+            }
+        }
+    }
+
+    private void IncreaseBallCount()
+    {
+        for (int i = 0; i < _gateNumber; i++)
+        {
+            GameObject _newBall =Instantiate(_ballPrefab);
+            _newBall.transform.SetParent(transform);
+            _newBall.GetComponent<SphereCollider>().enabled = false;
+            _newBall.gameObject.transform.localPosition = new Vector3(0f, 0f, _balls[_balls.Count - 1].transform.localPosition.z -1f);
+            _balls.Add(_newBall);
+ 
+        }
+    }
+
+    private void DecreaseBallCount()
+    {
+        for (int i = _balls.Count-1 ; i >= _targetCount; i--)
+        {
+            _balls[i].SetActive(false);
+            _balls.RemoveAt(i);
+            
         }
     }
 
--- Assets/Scripts/Gate_Controller.cs
+++ Assets/Scripts/Gate_Controller.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Random = UnityEngine.Random;
 
 public class Gate_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text _gateNumberText = null;
+    
+    [SerializeField] private enum GateType
     {
+        positiveGate,
         
+        negativeGate
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GateType _gateType;
+
+    [SerializeField] private int _gateNumber;
+
+    public int GetGateNumber()
     {
-        
+        return _gateNumber;
+    }   
+
+    void Start()
+    {
+        GenerateGateNumber();
     }
+
+    
+    private void GenerateGateNumber(){
+        switch (_gateType)
+        {
+            case GateType.positiveGate :
+                _gateNumber = Random.Range(1, 10);
+                _gateNumberText.text = _gateNumber.ToString();
+                                          break;
+            
+            case GateType.negativeGate :
+                _gateNumber = Random.Range(-10, -1);
+                _gateNumberText.text = _gateNumber.ToString();
+                                          break;
+        }
+    }
+    
 }
